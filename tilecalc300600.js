ff2c9f85d31459b312526fb9ca73185515a43534
WILLYJKIMINSEOUL <143001535+WILLYJKIMINSEOUL@users.noreply.github.com>
2023-08-24 09:44:32 +0900

    Update tilecalc300600.js

--- tilecalc300600.js
+++ tilecalc300600.js
@@ -2,10 +2,11 @@ document.getElementById('calculateBtn').addEventListener('click', function() {
   const width = parseFloat(document.getElementById('width').value);
   const height = parseFloat(document.getElementById('height').value);
   const depth = parseFloat(document.getElementById('depth').value);
+  const waste = parseFloat(document.getElementById('waste').value);
 
-  const wallTileCount = Math.ceil((width * depth * 2 / 1000000 / 1.44) + (height * depth * 2 / 1000000 / 1.44));
-  const floorTileCount = Math.ceil((width * height / 1000000 / 1.44));
+  const wallTileCount = Math.ceil(((width * height / 1000000 * 2 / 1.44) * (waste / 100 + 1)) + ((height * depth / 1000000 * 2 / 1.44) * (waste / 100 + 1)));
+  const floorTileCount = Math.ceil((width * height / 1000000 / 1.44) * (waste / 100 + 1));
 
   const resultElement = document.getElementById('result');
-  resultElement.innerHTML = `벽 타일 수량: ${wallTileCount}개<br>바닥 타일 수량: ${floorTileCount}개`;
+  resultElement.innerHTML = `벽 타일 수량: ${wallTileCount} 박스(1.44m² 기준)<br>바닥 타일 수량: ${floorTileCount} 박스(1.44m² 기준)`;
 });
